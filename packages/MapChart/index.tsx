799ed3c90abda0f5ad0fea4497da0b686a2cf662
linjx <2376909468@qq.com>
2024-09-18 18:20:03 +0800

    🚧 work in chart progress

--- packages/MapChart/index.tsx
+++ packages/MapChart/index.tsx
@@ -2,9 +2,12 @@ import React, { useEffect, useRef, useState } from "react";
 import { useImmer } from "use-immer";
 import { worldData } from "./worldData";
 
-const INCREASE = 4; //- 提高分辨率 放大 4 倍， 4k 分辨率： 3,840 x 2,160；
-const CANVAS_OFFSET = 5; //- 画布偏移量
-const SHAPE_SIZE = 5; //- 图形尺寸，在正方形时是宽高，其他图形为半径（中心点到顶点的距离）
+/** //- 提高分辨率 放大 4 倍， 4k 分辨率： 3,840 x 2,160； */
+const INCREASE = 4;
+/** //- 画布偏移量*/
+const CANVAS_OFFSET = 5;
+/** //- 图形尺寸，在正方形时是宽高，其他图形为半径（中心点到顶点的距离）*/
+const SHAPE_SIZE = 5;
 
 export interface MapChartProps {
   enableZoom?: boolean;
@@ -33,7 +36,7 @@ const MapChart: React.FC<MapChartProps> = (props) => {
   //- 控制鼠标滑轮放大
   const [scale, setScale] = useState(1);
   //- 控制拖拽偏移量
-  const [offset, setOffset] = useState({ x: 0, y: 0 });
+  const [translate, setTranslate] = useState({ x: 0, y: 0 });
   //- 判断是否处于拖拽状态
   const [dragging, setDragging] = useState(false);
   //- 记录鼠标位置
@@ -79,7 +82,12 @@ const MapChart: React.FC<MapChartProps> = (props) => {
       ctx.closePath();
     } else if (nodeShape === "2") {
       ctx.fillStyle = isHighLight ? "#F59506" : "#4C4640";
-      ctx.fillRect(x, y, SHAPE_SIZE * INCREASE, SHAPE_SIZE * INCREASE);
+      ctx.fillRect(
+        x,
+        y,
+        SHAPE_SIZE * INCREASE * scale,
+        SHAPE_SIZE * INCREASE * scale
+      );
       return;
     } else if (nodeShape === "3" || nodeShape === "5") {
       drawShape2(ctx, parseInt(nodeShape), x, y, -Math.PI / 2);
@@ -90,6 +98,19 @@ const MapChart: React.FC<MapChartProps> = (props) => {
     }
     ctx.fillStyle = isHighLight ? "#F59506" : "#4C4640";
     ctx.fill();
+  };
+
+  const limitTranslate = (prevOffset: { x: number; y: number }) => {
+    if (!canvasRef.current) return prevOffset;
+    const maxTranslateX =
+      canvasRef.current.width * scale - canvasRef.current.width;
+    const maxTranslateY =
+      canvasRef.current.height * scale - canvasRef.current.height;
+
+    return {
+      x: Math.min(0, Math.max(prevOffset.x, -maxTranslateX)),
+      y: Math.min(0, Math.max(prevOffset.y, -maxTranslateY)),
+    };
   };
 
   //* 画布修改
@@ -106,22 +127,23 @@ const MapChart: React.FC<MapChartProps> = (props) => {
     // 保存当前状态
     ctx.save();
 
-    // 应用缩放和平移
-    ctx.scale(scale, scale);
-    ctx.translate(Math.min(0, offset.x), Math.min(0, offset.y));
+    const tran = limitTranslate(translate);
+    ctx.translate(tran.x, tran.y);
 
     data.forEach(({ x, y, isHighLight }) => {
       drawShape(
         ctx,
-        (x + CANVAS_OFFSET) * INCREASE,
-        (y + CANVAS_OFFSET) * INCREASE,
+        (x + CANVAS_OFFSET) * INCREASE * scale,
+        (y + CANVAS_OFFSET) * INCREASE * scale,
         isHighLight
       );
     });
+    ctx.scale(scale, scale);
 
     // 恢复到原始状态
     ctx.restore();
-  }, [scale, offset, data, nodeShape]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scale, translate, data, nodeShape]);
 
   useEffect(() => {
     if (!defaultAction || defaultAction.length == 0) return;
@@ -133,42 +155,45 @@ const MapChart: React.FC<MapChartProps> = (props) => {
     );
   }, [defaultAction]);
 
+  //- 滚轮缩放
   useEffect(() => {
-    canvasRef.current?.addEventListener(
-      "wheel",
-      (ev) => {
-        // if (!enableZoom) return;
-        ev.preventDefault();
-
-        const scaleAmount = 0.1;
-        const newScale =
-          ev.deltaY > 0 ? scale - scaleAmount : scale + scaleAmount;
-        // console.log(newScale);
-        setScale(Math.max(0.1, Math.min(newScale, 5)));
-      },
-      {
-        passive: false,
-      }
-    );
+    function onWheel(this: HTMLCanvasElement, ev: WheelEvent) {
+      if (!enableZoom) return;
+      ev.preventDefault();
+      const scaleAmount = 0.1;
+      setScale((prev) =>
+        Math.max(
+          1,
+          Math.min(ev.deltaY > 0 ? prev - scaleAmount : prev + scaleAmount, 3)
+        )
+      );
+    }
+    const canvasDom = canvasRef.current;
+    if (!canvasDom) return;
+    canvasDom.addEventListener("wheel", onWheel, { passive: false });
+    return () => {
+      canvasDom.removeEventListener("wheel", onWheel);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [canvasRef]);
+  }, [canvasRef, enableZoom]);
 
   const handleMouseDown = (event: React.MouseEvent) => {
-    if (!enablePan) return;
     setDragging(true);
     setStart({ x: event.clientX, y: event.clientY });
   };
 
   const handleMouseMove = (event: React.MouseEvent) => {
-    if (!enablePan) return;
+    if (!enablePan || !canvasRef.current) return;
     if (dragging) {
       const dx = event.clientX - start.x;
       const dy = event.clientY - start.y;
 
-      setOffset((prevOffset) => ({
-        x: prevOffset.x + dx / scale,
-        y: prevOffset.y + dy / scale,
-      }));
+      setTranslate((prevOffset) =>
+        limitTranslate({
+          x: prevOffset.x + dx / scale,
+          y: prevOffset.y + dy / scale,
+        })
+      );
 
       setStart({ x: event.clientX, y: event.clientY });
     }
@@ -179,29 +204,50 @@ const MapChart: React.FC<MapChartProps> = (props) => {
     setDragging(false);
   };
 
-  //* 点击高亮元素，alt + 点击 添加 pin
+  const offset2canvas = (offset: number, x?: boolean) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return 0;
+    return (offset / (x ? 4016 : 2160)) * (x ? 1004 : 540) * scale;
+  };
+
+  //- 点击高亮元素，alt + 点击 添加 pin
   const handleCanvasClick = (event: React.MouseEvent) => {
     if (dragging) return;
     const canvas = canvasRef.current;
     if (!canvas) return;
 
     const canvasRect = canvas.getBoundingClientRect();
+    //* 鼠标点击位置 = 点击的位置 - 画布左/上距离窗口的距离
     const mouseX = event.clientX - canvasRect.left;
     const mouseY = event.clientY - canvasRect.top;
 
-    const offsetX = ((mouseX / canvasRect.width) * 4016) / INCREASE / scale;
-    const offsetY = ((mouseY / canvasRect.height) * 2160) / INCREASE / scale;
+    /**
+     * mouseX: 鼠标点击位置
+     * canvasRect.width: 画布元素（DOM 节点）宽度
+     * ! mouseX / canvasRect.width => 点击位置在画布的比例
+     * translate.x: 拖动偏移量
+     * canvas.width: 画布宽度（这里是画布的宽度，不是画布元素宽度，值为 4016）
+     * ! translate.x / canvas.width => 偏移量对于画布的比例
+     * 1004: 原始画布宽度（画布比例经过放大去适配4k，这里使用原始宽度：1004）
+     * scale: 缩放倍数
+     */
+    const offsetX =
+      ((mouseX / canvasRect.width - translate.x / canvas.width) * 1004) / scale;
+    const offsetY =
+      ((mouseY / canvasRect.height - translate.y / canvas.height) * 540) /
+      scale;
 
-    if (logPosition) {
-      console.log(`mouse position is: [${mouseX}, ${mouseY}]`);
-    }
-
+    //- alt + click
     if (event.altKey) {
       if (!enablePin) return;
       setPinPosition((draft) => {
-        draft.push([mouseX, mouseY]);
+        draft.push([
+          (mouseX - offset2canvas(translate.x, true)) / scale,
+          (mouseY - offset2canvas(translate.y, false)) / scale,
+        ]);
       });
     } else {
+      //- only click
       if (!enableAction) return;
       const newRectangles = data.map((rect, index) => {
         let positionX = [rect.x, rect.x + SHAPE_SIZE];
@@ -231,10 +277,19 @@ const MapChart: React.FC<MapChartProps> = (props) => {
 
   return (
     <div className="relative overflow-hidden">
+      <div className="absolute bottom-0 left-0 text-white bg-gray-400 select-none">
+        {scale}
+      </div>
+      <div className="absolute bottom-0 right-0 text-white bg-gray-400 select-none">
+        {JSON.stringify(translate)}
+      </div>
       {pinPosition?.map(([x, y], i) => (
         <div
           key={`${x}-${y}`}
-          style={{ left: x, top: y }}
+          style={{
+            left: x * scale + offset2canvas(translate.x, true),
+            top: y * scale + offset2canvas(translate.y, false),
+          }}
           onClick={(ev) => {
             ev.stopPropagation();
             setPinPosition((draft) => {
@@ -264,6 +319,7 @@ const MapChart: React.FC<MapChartProps> = (props) => {
         className="w-full h-full"
         onMouseUp={handleMouseUp}
         onClick={handleCanvasClick}
+        onMouseLeave={handleMouseUp}
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
       />
